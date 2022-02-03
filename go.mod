f753fc39893df3e5b839d49e1cd3ba136dcee0cf
flapan <mikkel@gadegaard.eu>
2022-02-02 23:52:13 -0800

    Changed router from gorilla mux to julien schmidt httprouter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/flapan/lenslocked.com
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
--- main.go
+++ main.go
@@ -4,30 +4,30 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send an email to: <a href=\"mailto:contact@lenslocked.com\">contact@lenslocked.com</a>.")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>This is our FAQ</h1>")
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	r := httprouter.New()
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.GET("/faq", faq)
+	r.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<p>We couldn't find the page you were looking for!</p>")
