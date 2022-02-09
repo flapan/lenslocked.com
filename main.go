c4f3bdf18bf07ad79387d44664288c8e2813ae0a
flapan <mikkel@gadegaard.eu>
2022-02-09 14:58:53 -0800

    Refactored main.go to route to signup page

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView, contactView *views.View
+var homeView, contactView, signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,13 +19,20 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
 }
 
