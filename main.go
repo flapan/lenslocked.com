c1dd6695b63fc4ede73a1134ea14cccd3084e903
flapan <mikkel@gadegaard.eu>
2022-02-25 22:31:04 -0800

    refactored router to utilize new users controller for signup

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"net/http"
 
+	"github.com/flapan/lenslocked.com/controllers"
 	"github.com/flapan/lenslocked.com/views"
 	"github.com/gorilla/mux"
 )
 
-var homeView, contactView, signupView *views.View
+var homeView, contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,20 +20,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	userC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", userC.New)
 	http.ListenAndServe(":3000", r)
 }
 
