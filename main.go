9def695a4771641ce0ee95be2a2d100c0a9a8eb3
flapan <mikkel@gadegaard.eu>
2022-03-04 23:22:53 -0800

    Moved static pages to static controller and views/static folder

--- main.go
+++ main.go
@@ -4,30 +4,16 @@ import (
 	"net/http"
 
 	"github.com/flapan/lenslocked.com/controllers"
-	"github.com/flapan/lenslocked.com/views"
 	"github.com/gorilla/mux"
 )
 
-var homeView, contactView *views.View
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticC := controllers.NewStatic()
 	userC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", userC.New).Methods("GET")
 	r.HandleFunc("/signup", userC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
