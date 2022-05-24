d1ac0285953190e9fb7b7d6cdfa2db28ae78c50c
flapan <mikkel@gadegaard.eu>
2022-05-23 23:07:21 -0700

    Added gorm for DB

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -14,29 +14,25 @@ const (
 	dbname = "lenslocked_dev"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Color string
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	rows, err := db.Query(`
-		SELECT * FROM users INNER JOIN orders
-		ON users.id = orders.user_id`)
-	if err != nil {
-		panic(err)
-	}
-	for rows.Next() {
-		var userID, orderID, amount int
-		var email, name, description string
-		if err := rows.Scan(&userID, &name, &email, &orderID, &userID, &amount, &description); err != nil {
-			panic(err)
-		}
-		fmt.Println("userID:", userID, "name:", name, "email:", email, "orderID:", orderID, "amount:", amount, "description:", description)
-	}
-	if rows.Err() != nil {
-		panic(rows.Err())
-	}
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	var u User
+	db.First(&u)
+	fmt.Println(u)
 }
--- go.mod
+++ go.mod
@@ -7,3 +7,8 @@ require github.com/gorilla/mux v1.8.0
 require github.com/gorilla/schema v1.2.0
 
 require github.com/lib/pq v1.10.4
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
