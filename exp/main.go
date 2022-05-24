7442d495de834bcbaea78cc06d9e2dad7fc8c324
flapan <mikkel@gadegaard.eu>
2022-05-23 23:22:07 -0700

    Added order table and data

--- exp/main.go
+++ exp/main.go
@@ -10,29 +10,55 @@ import (
 const (
 	host   = "localhost"
 	port   = 5432
-	user   = "mikkel"
+	dbuser = "mikkel"
 	dbname = "lenslocked_dev"
 )
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
-	Color string
+	Name   string
+	Email  string `gorm:"not null;unique_index"`
+	Color  string
+	Orders []Order
+}
+
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
 }
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-		host, port, user, dbname)
+		host, port, dbuser, dbname)
 	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	db.LogMode(true)
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Order{})
 
 	var u User
-	db.First(&u)
+	if err := db.First(&u).Error; err != nil {
+		panic(err)
+	}
+	createOrder(db, u, 1001, "Fake description #1")
+	createOrder(db, u, 9999, "Fake description #2")
+	createOrder(db, u, 100, "Fake description #3")
+
 	fmt.Println(u)
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	err := db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	}).Error
+
+	if err != nil {
+		panic(err)
+	}
 }
