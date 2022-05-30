77429d20363d8838f84f760828f3035ed8d68a00
flapan <mikkel@gadegaard.eu>
2022-05-30 11:35:03 -0700

    Hooked up MVC chain for user signup

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/flapan/lenslocked.com/controllers"
+	"github.com/flapan/lenslocked.com/models"
 	"github.com/gorilla/mux"
 )
 
+const (
+	host   = "localhost"
+	port   = 5432
+	dbuser = "mikkel"
+	dbname = "lenslocked_dev"
+)
+
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		host, port, dbuser, dbname)
+	us, err := models.NewUserService(psqlInfo)
+	must(err)
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
-	userC := controllers.NewUsers()
+	userC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
