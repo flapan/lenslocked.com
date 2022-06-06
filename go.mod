1f91989e172b38133bc8b043298adb6a126f5fb9
flapan <mikkel@gadegaard.eu>
2022-06-05 21:48:33 -0700

    hashing password and storing it, while resetting user.password to avoid it spilling into logs

--- go.mod
+++ go.mod
@@ -11,4 +11,5 @@ require github.com/lib/pq v1.10.4
 require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 )
--- models/users.go
+++ models/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -65,7 +66,14 @@ func first(db *gorm.DB, dst interface{}) error {
 }
 
 // Creates the provided user and backfill data like ID, created_at etc.
+// Naive hashing of password, i.e no checking for length etc
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
@@ -106,6 +114,8 @@ func (us *UserService) AutoMigrate() error {
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
