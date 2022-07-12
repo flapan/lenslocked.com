319ec713cc55ca09bd91d883ae0ead0bf71c97e4
flapan <mikkel@gadegaard.eu>
2022-07-11 22:33:03 -0700

    Created gallery create action and fixed bug creating NewGalleries (had omitted the GalleryService from the returned struct, leading to a nil pointer

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/flapan/lenslocked.com/models"
 	"github.com/flapan/lenslocked.com/views"
 )
@@ -8,10 +12,40 @@ import (
 func NewGalleries(gs models.GalleryService) *Galleries {
 	return &Galleries{
 		New: views.NewView("bootstrap", "galleries/new"),
+		gs:  gs,
 	}
 }
 
 type Galleries struct {
 	New *views.View
 	gs  models.GalleryService
+}
+
+type GalleryForm struct {
+	Title string `schema:"title"`
+}
+
+// Create is used to process the galleries form when a user submits it.
+// This is used to create a new gallery.
+//
+// POST /galleries
+func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
+	var form GalleryForm
+
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.SetAlert(err)
+		g.New.Render(w, vd)
+		return
+	}
+	gallery := models.Gallery{
+		Title: form.Title,
+	}
+	if err := g.gs.Create(&gallery); err != nil {
+		vd.SetAlert(err)
+		g.New.Render(w, vd)
+		return
+	}
+	fmt.Fprintln(w, gallery)
 }
--- models/galleries.go
+++ models/galleries.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 // Gallery represents the gallery model in the DB
 type Gallery struct {
